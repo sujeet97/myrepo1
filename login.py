ef86d399e7531b8190405c4ccc3c13092e70b8c3
sujeet97 <sujeet.kumar1197@gmail.com>
2020-09-16 15:55:43 +0000

    login.py

---
+++ login.py
@@ -0,0 +1,17 @@
+class login:
+ def __init__(self,id,pas):
+     self.id="Sujeet"
+     self.pas="myname"
+  
+ def check(id,pas):
+        print self.id
+        print lod.id
+        if(self.id==log.id and self.pas==log.pas):
+            print "Login success!"
+
+
+log=login("","")
+log.check(raw_input("Enter Login ID:"),
+        input("Enter password: "))
+
+print "Login Page"  
